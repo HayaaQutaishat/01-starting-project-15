283c67528566a8fcf674edeb767a5accadb6a07a
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2023-01-02 14:18:24 +0100

    Log the user out when the token is expired

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -7,19 +7,34 @@ const AuthContext = React.createContext({
   logout: () => {},
 });
 
+const calculateRemainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime();
+  // convert expirationTime to Date object (and get it in millisecond)
+  const adjExpirationTime = new Date(expirationTime).getTime();
+
+  const remainingDuration = adjExpirationTime - currentTime;
+
+  return remainingDuration;
+};
+
 export const AuthContextProvider = (props) => {
   // when this app starts look in Local Storage and see if I find a Token
   const initialToken = localStorage.getItem("token");
   const [token, setToken] = useState(initialToken);
   const isLoggedIn = !!token;
 
-  const loginHandler = (token) => {
-    setToken(token);
-    localStorage.setItem("token", token);
-  };
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem("token");
+  };
+
+  const loginHandler = (token, expirationTime) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+
+    const remainingTime = calculateRemainingTime(expirationTime);
+
+    setTimeout(logoutHandler, remainingTime);
   };
 
   const contextValue = {
