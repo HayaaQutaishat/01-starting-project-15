88acf8810f0794617c9b6f20ac52cd545e457237
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2023-01-02 12:00:38 +0100

    Preventing Browser Default behaviour

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,20 @@
-import classes from './ProfileForm.module.css';
+import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+  const changePwFormHandler = (event) => {
+    event.preventDefault();
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={changePwFormHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
