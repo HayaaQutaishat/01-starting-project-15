0885f28ae113b8f381c1380e536b631339bf6fc7
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2023-01-02 12:52:28 +0100

    Render UserProfile Route only if user is  LoggedIn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import { Switch, Route } from 'react-router-dom';
-
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import { Switch, Route } from "react-router-dom";
+import { useContext } from "react";
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import AuthContext from "./store/auth-context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
+  const userIsLoggedIn = authCtx.isLoggedIn;
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
+        <Route path="/" exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+        <Route path="/auth">
           <AuthPage />
         </Route>
-        <Route path='/profile'>
-          <UserProfile />
-        </Route>
+        {userIsLoggedIn && (
+          <Route path="/profile">
+            <UserProfile />
+          </Route>
+        )}
       </Switch>
     </Layout>
   );
