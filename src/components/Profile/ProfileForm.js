8cc8d316f390720e83600a81ca4d11978eaedc37
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2023-01-02 12:06:34 +0100

    Collect the entered Password using 'useRef'

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,14 +1,20 @@
 import classes from "./ProfileForm.module.css";
+import { useRef } from "react";
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+
   const changePwFormHandler = (event) => {
     event.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+    console.log(enteredNewPassword);
   };
   return (
     <form className={classes.form} onSubmit={changePwFormHandler}>
       <div className={classes.control}>
         <label htmlFor="new-password">New Password</label>
-        <input type="password" id="new-password" />
+        <input type="password" id="new-password" ref={newPasswordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
