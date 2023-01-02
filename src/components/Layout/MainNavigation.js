c7eed08954cdca93fb4c3d9b90fdea00afd3039b
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2023-01-02 12:44:11 +0100

    Adding Logout logic

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -7,6 +7,10 @@ const MainNavigation = () => {
   const authCtx = useContext(AuthContext);
   const isLoggedIn = authCtx.isLoggedIn;
 
+  const logUserOutHandler = () => {
+    authCtx.logout();
+  };
+
   return (
     <header className={classes.header}>
       <Link to="/">
@@ -26,7 +30,7 @@ const MainNavigation = () => {
           )}
           {isLoggedIn && (
             <li>
-              <button>Logout</button>
+              <button onClick={logUserOutHandler}>Logout</button>
             </li>
           )}
         </ul>
